c702d80b0681c627d26b1b29fba05768fd3cee89
Mikko Liivak <mll286@cornell.edu>
2024-08-29 14:09:18 -0400

    connected sliders to params

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -39,6 +39,11 @@ private:
     
     CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
     
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+    
+    Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment, lowCutFreqSliderAttachment, highCutFreqSliderAttachment, lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
+    
     std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ThreeQAudioProcessorEditor)

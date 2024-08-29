6eba478a2d17c65b83602f8c65e0260025680172
Mikko Liivak <mll286@cornell.edu>
2024-08-29 13:54:26 -0400

    added sliders to gui

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -181,8 +181,8 @@ bool ThreeQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* ThreeQAudioProcessor::createEditor()
 {
-    // return new ThreeQAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new ThreeQAudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -191,12 +191,21 @@ void ThreeQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void ThreeQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
